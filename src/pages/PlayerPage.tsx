7d789468056d95ebedaa6a4c7e6ffd2dd7934e49
Camille Sylla <camillesylla.web@gmail.com>
2024-09-11 13:15:36 +0200

    feat(player-page): get player info from api based on player id

--- src/pages/PlayerPage.tsx
+++ src/pages/PlayerPage.tsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Player } from "../../types/players";
 
 export default function PlayerPage() {
   const { id } = useParams();
-  return <>{id}</>;
+  const [player, setPlayer] = useState<Player | null>(null);
+  // const [playerStats, setPlayerStats] = useState({});
+  useEffect(() => {
+    try {
+      fetch(`${import.meta.env.VITE_API_BASE_URL}/players/${id}`, {
+        method: "GET",
+        headers: {
+          Authorization: import.meta.env.VITE_API_KEY,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPlayer(data.data);
+        });
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        "Something occured during data fetching, please comeback later",
+      );
+    }
+  }, [id]);
+  return (
+    <>
+      <div>
+        {player ? (
+          <>
+            <h1>
+              {player.first_name} {player.last_name}
+            </h1>
+          </>
+        ) : (
+          "pending..."
+        )}
+      </div>
+    </>
+  );
 }

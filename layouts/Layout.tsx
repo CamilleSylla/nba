06e22ccbd30a4240f367fb72abde6b5212068b10
Camille Sylla <camillesylla.web@gmail.com>
2024-09-11 14:21:38 +0200

    feat(player-page): display data table for player stats

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
     setQuery(e.target.value.toLowerCase());
   };
   return (
-    <div className="w-3/4 my-0 mx-auto py-10 space-y-5">
+    <div className="w-[90%] my-0 mx-auto py-10 space-y-5">
       <div className="flex items-center gap-5 w-1/2 p-2 rounded-lg border-2 border-slate-400">
         <MagnifyingGlassIcon className="size-5" />
         <input type="text" className="flex-1" onChange={handleChange} />
--- src/pages/PlayerPage.tsx
+++ src/pages/PlayerPage.tsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Player } from "../../types/players";
 
 export default function PlayerPage() {
   const { id } = useParams();
   const [player, setPlayer] = useState<Player | null>(null);
-  // const [playerStats, setPlayerStats] = useState({});
+  const [playerStats, setPlayerStats] = useState([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_pending, setPending] = useState<boolean>(true);
+  const tableColumns = useMemo(() => {
+    return playerStats.length > 0 ? Object.keys(playerStats?.[0]) : [];
+  }, [playerStats]);
   useEffect(() => {
     try {
+      //fetching player profile data
       fetch(`${import.meta.env.VITE_API_BASE_URL}/players/${id}`, {
         method: "GET",
         headers: {
@@ -18,6 +24,33 @@ export default function PlayerPage() {
         .then((data) => {
           setPlayer(data.data);
         });
+
+      //fetching player stats data
+      fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/stats?seasons[]=2023&player_ids[]=${id}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: import.meta.env.VITE_API_KEY,
+          },
+        },
+      )
+        .then((res) => res.json())
+        .then(({ data }) => {
+          const stats = data.reduce((acc, item) => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const { player, team, game, ...stats } = item;
+            delete stats.id;
+            if (!acc) {
+              acc = [{ date: game.date, ...stats }];
+            } else {
+              acc = [...acc, { date: game.date, ...stats }];
+            }
+            return acc;
+          }, []);
+          setPlayerStats(stats);
+        });
+      setPending(false);
     } catch (error) {
       console.error(error);
       throw new Error(
@@ -33,6 +66,36 @@ export default function PlayerPage() {
             <h1>
               {player.first_name} {player.last_name}
             </h1>
+            <table className="table-fixed border-collapse rounded-lg px-5 text-center w-full overflow-hidden">
+              <thead className="bg-gray-200">
+                <tr>
+                  {tableColumns.map((key: string, i: string | number) => {
+                    return (
+                      <th key={"tr" + key + i} className="h-14">
+                        {key.toUpperCase()}
+                      </th>
+                    );
+                  })}
+                </tr>
+              </thead>
+              <tbody className="">
+                {playerStats.map((stats, row) => {
+                  return (
+                    <tr className="border-b border-slate-400 hover:bg-slate-200 transition-all duration-300 ">
+                      {Object.keys(stats).map((key, i) => {
+                        return (
+                          <td key={row + i + key}>
+                            {key !== "date"
+                              ? Math.round(playerStats[row][key] * 100) / 100
+                              : playerStats[row][key]}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </>
         ) : (
           "pending..."
